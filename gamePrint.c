52a6a992d279b312c12c0509ba6d2c424505a224
kju1025 <kimjiunn06@tukorea.ac.kr>
2025-07-17 17:07:53 +0900

    feat: file test

---
+++ gamePrint.c
@@ -0,0 +1,19 @@
+#include "gamePrint.h"
+
+int main(void) {
+    FILE* fp = fopen("Tutorial.txt", "r");
+ 
+    if (fp == NULL) {
+        printf("파일을 열 수 없습니다.\n");
+        return 1;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
